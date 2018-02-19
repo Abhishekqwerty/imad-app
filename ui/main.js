3b5a061ddfb4b062c0551f25837fba791bbb91e0
Abhishekqwerty <gaikwadabhishek978@gmail.com>
2018-02-19 12:45:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,4 @@
-console.log('Loaded!');
-
-var newContent = document.getElementById("main-content");
-
-newContent.innerHTML = "New Content";
-
-var img = document.getElementById("coder");
-
-var marginLeft= 0;
-function moveRight (){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-
-img.onclick = function(){
-    var interval = setInterval(moveRight,50);
+var button = document.getElementById('counter');
+button.onclick = function() {
+    
 };
