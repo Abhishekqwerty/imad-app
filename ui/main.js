9ac22760eaac3f41fc7a9b487ce01207b6294e58
Abhishekqwerty <gaikwadabhishek978@gmail.com>
2018-02-19 13:43:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,24 @@
  var button = document.getElementById('counter');
- var counter = 0;
+ 
  button.onclick = function () {
     
+    //create a request 
+    var request = new XMLHttpRequest();
     
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    //capture the counter and replace it
+    
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE){
+            
+            if(request.status === 200){
+                var counter = request.responseText;               
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    
+    request.open('GET', 'http://gaikwadabhishek978.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
  };
